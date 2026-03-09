63e781007070ba42d7ae22b1c15583b1b0d78195
Pranav Deshakulkarni Manjunath <pranavdeshakulkarni@gmail.com>
2026-03-08 22:11:53 -0400

    Setting up

---
+++ src/reactive_nav/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'reactive_nav'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pranavdm',
+    maintainer_email='pranavdm@umd.edu',
+    description='Reactive navigation with teleoperation and autonomous behavior',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'teleop_node = reactive_nav.teleop_node:main',
+            'autonomous_nav_node = reactive_nav.autonomous_nav_node:main',
+        ],
+    },
+)
